e0b1bd55668b4bdddf02ac29ba8a56b6012bcc65
Oussama Teyib <oussama.teyib@gmail.com>
2023-04-11 02:23:07 +0000

    Formatting output

--- rsrcer.c
+++ rsrcer.c
@@ -1,20 +1,22 @@
-// convert a resource file into a C source file array.
+// Convert a resource file into a C source file array.
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
     if (argc != 4)
     {
-        fprintf(stderr, "Usage: input_file output_file array_name");
-        return -1;
+        fprintf(stderr, "Invalid number of parametres!\n");
+        fprintf(stderr, "Usage: rsrcer <input_file> <output_file> <array_name>");
+        return EXIT_FAILURE;
     } 
 
     FILE* input = fopen(argv[1], "rb");
     if (!input)
     {
         fprintf(stderr, "ERROR: cannot open the file!\n");
-        return -1;
+        return EXIT_FAILURE;
     }
        
     // get the size of the input file
@@ -27,23 +29,27 @@ int main(int argc, char* argv[])
     {
         fprintf(stderr, "ERROR: cannot create a file!\n");
         fclose(input);
-        return -1;
+        return EXIT_FAILURE;
     }   
 
     unsigned char byte;
-    int count = 0;
-    fprintf(output, "unsigned char %s[] = {", argv[3]); 
+    size_t count = 0;
+    fprintf(output, "const unsigned char %s[] = {", argv[3]); 
     while (1 == fread(&byte, 1, 1, input))
     {
-        count++;
+        if (!(count % 16))
+            fprintf(output, "\n%*c", 8, ' ');
+ 
         fprintf(output, "0x%02X", byte);
-        if (count < size)
+        if (count < size - 1) // -1 to avoid the last byte
             fprintf(output, ", ");
+        count++;
     }
     fprintf(output, "};\n");
-    
-    printf("%d bytes successfully printed!\n", count);
+    fprintf(output, "const size_t %s_size = %zu;\n", argv[3], count);
+
+    printf("%zu bytes successfully printed!\n", count);
     fclose(input);
     fclose(output);
-    return 0;
+    return EXIT_SUCCESS;
 }
